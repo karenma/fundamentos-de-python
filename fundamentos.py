945d01243f113656fe752a1faca3a4c2ec992671
karenma <anakrn_08@hotmail.com>
2025-06-02 16:22:28 -0600

    se agregaron operadores logicos y de comparacion
    se agrego los operadores lógicos y comparación

---
+++ fundamentos.py
@@ -0,0 +1,33 @@
+if __name__=="__main__":
+    print ("----operadores aritmeticos.------")
+    print ("Operador suma 3+5=", 3 + 5)
+    print ("Operador resta 9-8=", 9 + 8)
+    print ("Operador multiplicacion  3*5=", 3 * 5)
+    print ("Operador division 3/5=", 3 / 5)
+    print ("Operador division entera 3//5=", 3 // 5)
+    print ("Operador residuo 10%3", 10%3)
+    print ("Operador Exponente 3^5=", 3 ^5)
+    #este comentario de una sola linea
+    '''este es un comenrario 
+    de varias lineas'''
+    print("---operadored logicos")
+    #tablas de verdad de and
+    print("True and Tue:",True and True)
+    print("True and False:",True and False)
+    print("False and True:",False and True)
+    print("False and False:",False and False)
+    #tablas de verdad de or
+    print("True or Tue:",True or True)
+    print("True or False:",True or False)
+    print("False or True:",False or True)
+    print("False or False:",False or False)
+    #tablas de verdad de not
+    
+    print("not False:",not False)
+    print("not True:",not True)
+   
+   print("---operadored de comparacion")
+    print("---operadored logicos")
+
+
+
